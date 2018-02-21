7e5aa4cd86db2f9ab6f559b7a630316bb5e7b591
Sumithran <sumithranvaikom@gmail.com>
2018-02-21 16:26:47 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,17 +1,14 @@
 <?php
 error_reporting( E_ALL & ~E_DEPRECATED & ~E_NOTICE );
-// Establishing connection with server by passing "server_name", "user_id", "password" 
-  $conn = mysqli_connect("localhost", "root", "","lotus");
-
-// Selecting Database by passing "nightfury" and above connection variable
-
+// Establishing connection with server by passing "server_name", "user_id", "password" ,"database_name"
+  $conn = mysqli_connect("server_name", "user_id", "password","database_name");
 
 //Fetching Values from URL  
 $name  =$_POST['Name'];
 $msg   =$_POST['Message'];
 
 //Insert query 
-	$sql = "insert into trash (name,msg) values ('$name','$msg')";
+	$sql = "insert into formsub (name,msg) values ('$name','$msg')";
   $query = mysqli_query( $conn,$sql);
   if($query){
   echo "Working good"; 
